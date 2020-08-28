89248d02b5de32b7f8cbc31b08c49b871d5b2a64
Bhavin <bhavinv@hyperlinkinfosystem.net.in>
2020-08-28 19:21:49 +0530

    Initial Commit

---
+++ TXIS-SDK/TXIS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  TXIS_SDK.h
+//  TXIS-SDK
+//
+//  Created by hyperlink on 28/08/20.
+//  Copyright © 2020 Hyperlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TXIS_SDK.
+FOUNDATION_EXPORT double TXIS_SDKVersionNumber;
+
+//! Project version string for TXIS_SDK.
+FOUNDATION_EXPORT const unsigned char TXIS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TXIS_SDK/PublicHeader.h>
+
+
